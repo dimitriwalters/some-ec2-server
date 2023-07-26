ce2a9c4fa1197a02c03aa374e5197bd71e1ef6bf
Dimitri Walters <me@dimi.io>
2023-07-26 20:54:18 +0000

    use count backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { count: null };
+  callApi = async () => {
+    const response = await fetch("/api/count")
+    const body = await response.json();
+    if (response.status != 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ count: res.count }))
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            This page has been visited {this.state.count} times.
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
